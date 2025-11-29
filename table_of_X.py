e93cd3822c470b9d0cfb935d8472461f39b28a91
assisstent2025-cloud <assisstent2025@gmail.com>
2025-11-29 10:20:27 +0300

    Update table_of_X.py

--- table_of_X.py
+++ table_of_X.py
@@ -2,8 +2,8 @@ a = 11
 
 for i in range(1, a):
     for j in range(1, a):
-        print(i * j, end='__')
-    print('|')
+        print(i * j, end='  ')
+    print('')
 
 # НЕ ПОНИМАЮ ТОЛЬКО, КАК ЕЕ ПОДРОВНЯТЬ.
 # С ПРОБЕЛАМИ ЭКСПЕРЕМЕНТЫ ПРОВАЛИЛИСЬ)))
